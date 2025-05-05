bb63e7667b43c8df09c8e03a1a2597e63530f630
Arthur <briotarthur@gmail.com>
2025-05-05 17:54:20 +0200

    Ajout texte

--- src/app/veille/page.jsx
+++ src/app/veille/page.jsx
@@ -5,19 +5,11 @@ const VeillesPage = () => {
     const veilles = [
         {
             id: 1,
-            sujet: "L'Impact de la Big Data sur les Entreprises",
-            description: "Le Big Data a un impact profond et transformateur sur divers aspects de la société et des industries. ",
+            sujet: "L'Impact de la Big Data dans le monde ",
+            description: "Le Big Data a un impact profond et transformateur sur divers aspects de la société et des industries.",
             source: "https://www.lefigaro.fr/tag/big-data",
             sourceText: "Big Data",
             date: "Janvier 2025"
-        },
-        {
-            id: 2,
-            sujet: "L'Intelligence Artificielle",
-            description: "L'article du Figaro explore l'impact de l'intelligence artificielle sur divers secteurs, de la recherche scientifique à la sécurité, en passant par les défis éthiques et les innovations technologiques.",
-            source: "https://www.lefigaro.fr/tag/intelligence-artificielle",
-            sourceText: "IA", // Texte court pour le lien
-            date: "Décembre 2024"
         },
     ];
 
